c28e6012ee3fa580ba765cf6213fdc327541558d
SuperFola <lexplt.dev@gmail.com>
2020-08-28 18:10:54 +0200

    updating servers command

--- bot/commands/Owner/servers.js
+++ bot/commands/Owner/servers.js
@@ -6,13 +6,16 @@ exports.run = (client, msg, args) => {
         page = parseInt(args[0]);
 
     let servers = [];
+    let total_members = 0;
     client.guilds.cache.forEach(guild => {
         let owner = guild.members.cache.get(guild.ownerID).user.username;
-        if (guild.available)
+        if (guild.available) {
             servers.push({
                 name: `${guild.name} owned by ${owner}`,
                 value: `${guild.memberCount} members`,
             });
+            total_members += guild.memberCount;
+        }
     });
 
     if (servers.length < page * 10) {
@@ -25,6 +28,9 @@ exports.run = (client, msg, args) => {
             title: 'Servers',
             color: 0x8f00b3,
             description: `Currently in ${client.guilds.cache.size} servers`,
+            footer: {
+                text: `Total members reached: ${total_members}`,
+            },
         },
     };
 
