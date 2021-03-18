676f614a105008088955ed173ece0c4218e4742d
Alexandre Plateau <lexplt.dev@gmail.com>
2021-03-18 13:44:55 +0100

    enhancing activity command

--- bot/commands/Owner/activity.js
+++ bot/commands/Owner/activity.js
@@ -16,7 +16,9 @@ exports.run = (client, msg, args) => {
     let type = 'PLAYING';
     if (args[0].startsWith('type=')) {
         type = args[0].substr('type='.length, args[0].length - 'type='.length);
-        args.shift();  // remove first element of args
+        if (!authorized.includes(type))
+            return msg.channel.send(`:x: Invalid activity type: ${type}`);
+        args.shift(); // remove first element of args
     }
 
     if (args.length < 1 || !authorized.includes(type))
